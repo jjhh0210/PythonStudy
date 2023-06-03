f6c28f6fb2d327d307242c6194b7ac90478e883b
jjhh0210 <jjhh0210@ewhain.net>
2023-06-03 16:17:02 +0900

    fix : algorithm_practice - 가장높은탑쌓기, 최대선연결하기

--- practice_src/algorithm_practice/가장높은탑쌓기.py
+++ practice_src/algorithm_practice/가장높은탑쌓기.py
@@ -3,15 +3,17 @@ bricks=[]
 for i in range(n):
     a, b, c=map(int, input().split())
     bricks.append((a, b, c))
-bricks.sort(reverse=True)
-dy=[0]*n
-dy[0]=bricks[0][1]
-res=bricks[0][1];
-for i in range(1, n):
-    max_h=0;
-    for j in range(i-1, -1, -1):
-        if bricks[j][2]>bricks[i][2] and dy[j]>max_h:
-            max_h=dy[j]
-    dy[i]=max_h+bricks[i][1]
-    res=max(res, dy[i])
+bricks.sort(reverse=True)   #맨 앞에 잇는 밑면의 넓이에 대해 역순으로 정렬
+
+dh = [0]*n # 현재까지의 최대 높이 저장
+dh[0] = bricks[0][1]
+res = 0     #최대 탑 높이
+for i in range(1,n):
+    max_h = 0
+    for j in range(i,-1,-1):
+        if bricks[i][2] <= bricks[j][2] and dh[j] > max_h:# 나보다 무게 무거운 것들 중 최대 탑 높이 높은애들 고르기
+            max_h = dh[j]
+    dh[i] = max_h + bricks[i][1]
+    res = max(res,dh[i])
+
 print(res)
--- practice_src/algorithm_practice/최대선연결하기.py
+++ practice_src/algorithm_practice/최대선연결하기.py
@@ -1,15 +1,33 @@
-n=int(input())
-arr=list(map(int, input().split()))
+## 최대부분증가수열 문제와 같음
+## 역추적을 통해 수열 출력해보기
+n = int(input())
+arr = list(map(int, input().split()))
 arr.insert(0,0)
-dy=[0]*(n+1)
-dy[1]=1
-res=0
-for i in range(2, n+1):
-    max=0
-    for j in range(i-1, 0, -1):
-        if arr[j]<arr[i] and dy[j]>max:
-            max=dy[j]
-    dy[i]=max+1
+dy = [0]*(n+1)  #메모이제이션
+prev = [0]*(n+1)  # 이전 위치 기록
+dy[1] = 1
+res = 0
+last_index = 0  # 최대 부분 증가수열의 마지막 위치
+
+for i in range(2,n+1):
+    max_val = 0
+    for j in range(i-1,0,-1):
+        if arr[j] < arr[i] and dy[j]>max_val:
+            max_val = dy[j]
+            prev[i] = j  # j 위치에서 i로 왔다고 기록
+    dy[i] = max_val+1
+    # 최대증가수열의 길이
     if dy[i]>res:
-        res=dy[i]
+        res = dy[i]
+        last_index = i  # 최대 길이가 갱신되면 마지막 위치 갱신
+
+# 최대 부분 증가수열 추출
+seq = []
+while last_index != 0:
+    seq.append(arr[last_index])
+    last_index = prev[last_index]  # 이전 위치로 이동
+seq.reverse()  # 역순으로 얻었으므로 뒤집기
+
 print(res)
+print(' '.join(map(str, seq)))
+print(prev)
