3c353a1c2c0c842c2d95e0849dfd804878331c11
jjhh0210 <jjhh0210@github.com>
2023-10-17 16:35:46 +0900

    update : BOJ 1874_스택수열

---
+++ practice_src/baekjoon/1874_스택수열.py
@@ -0,0 +1,39 @@
+# 1~n 차례대로 push해야됨
+
+def solution(n,arr):
+    stack = []
+    answer = []
+    maxx = max(arr)
+    point = 0
+    for num in range(1,maxx+1):
+        stack.append(num)
+        answer.append("+")
+        while len(stack)>0 and stack[-1]==arr[point]:
+            stack.pop()
+            answer.append("-")
+            point+=1
+
+    while stack:
+        if stack[-1] == arr[point]:
+            stack.pop()
+            answer.append("-")
+            point+=1
+        else:
+            return 0
+
+    return answer
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = [0]*n
+    for i in range(n):
+        arr[i] = int(input())
+    answer = solution(n,arr)
+    if answer==0:
+        print("NO")
+    else:
+        for a in answer:
+            print(a)
+
+
