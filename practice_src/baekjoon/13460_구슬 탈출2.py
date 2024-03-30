bfb6b65fb559cd0a1731314cb1026d7e20c2715b
jjhh0210 <parkjihyun0210@gmail.com>
2024-03-30 16:51:28 +0900

    update: BOJ 13460_구슬탈출2 - 시뮬레이션

--- practice_src/baekjoon/13460_구슬 탈출2.py
+++ practice_src/baekjoon/13460_구슬 탈출2.py
@@ -1,3 +1,44 @@
+
+def move(ci,cj,di,dj):
+
+    return ci,cj
+def dfs(ri,rj,bi,bj,cnt):
+    # 두개 위치 다르고, R이 exit인 경우
+    global N,M,ans
+    if maps[ri][rj]=='O':
+        ans = cnt
+        return
+    # 탐색 시작
+    for di, dj in dirs:
+        # 1. 사탕 옮기기
+        # 1-1) 탐색할 방향 기준 앞서있는 사탕 먼저 옮기기
+
+
+
+        # 2. 둘다 exit이면 탐색X
+        # 3. 다음 방향 호출
+
+
+
+import sys
+input = sys.stdin.readline
+
+N,M = map(int, input().split())
+maps = [ input().strip() for _ in range(N)]
+
+dirs = [(-1,0), (1,0), (0,-1), (0,1)]  # 위, 아래 , 왼, 오
+ri,rj,bi,bj = 0,0,0,0
+ans=0
+# 최초 사탕 위치 찾기
+for i in range(1,N):
+    for j in range(1,M):
+        if maps[i][j] == 'R':
+            ri,rj = i,j
+        if maps[i][j] == 'B':
+            bi,bj = i,j
+
+dfs(ri,rj,bi,bj, 0)
+
 '''
 코드트리- 2개의 사탕 / 2015년 하반기 삼성 그룹 오후 2번
 
@@ -17,4 +58,3 @@ maps
 빨간색 사탕 빼내기 위해 기울이는 최소 횟수
 - if 기울이는 최소 횟수 > 10 : -1
 '''
-
