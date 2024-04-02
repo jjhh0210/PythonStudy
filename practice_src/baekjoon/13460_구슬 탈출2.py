8cec68d86e54a3c722751d0ea506573707e52bc7
jjhh0210 <parkjihyun0210@gmail.com>
2024-04-02 15:46:16 +0900

    update: BOJ 13460_구슬탈출2 - 시뮬레이션 finished

--- practice_src/baekjoon/13460_구슬 탈출2.py
+++ practice_src/baekjoon/13460_구슬 탈출2.py
@@ -8,54 +8,70 @@ def select_first(ri,rj,bi,bj,d):
     else:           # 오른쪽 이동 -> j가 더 큰 것이 먼저 이동
         return 'R' if rj>bj else 'B'
 
-def move_candy(ci,cj,di,dj):
-    is_exited = False
-    # 이동 멈추는 경우 -> exit이 나옴 / 벽이 나옴 / 다른 사탕이 나옴 =>즉, 빈칸일때만 이동 가능
-    # 빈칸일 동안만 이동하다가, exit이 나오면? or 벽이나 다른 사탕이 나오면? 나누어서 사탕 최종 위치 처리
-    while maps[ci][cj] == '.':
-        
+def move_candy(ci,cj,di,dj,moving_candy):
+    # 4가지 경우 -> 빈칸 나옴 / exit이 나옴 / 벽이나 다른 사탕이 나옴 => 사탕 최종 위치 반환
 
-    return ci,cj,is_exited
+    maps[ci][cj] = '.'  # 우선 현재 사탕 칸을 빈 칸으로 변경
+    while True:
+        if maps[ci + di][cj + dj] == '.':   # 빈칸이 나오면? : 움직이고 계속
+            ci,cj = ci+di,cj+dj
+            continue
+        elif maps[ci+di][cj+dj]== 'O':      # exit이 나오면? : 움직이고 끝!! 사탕 표시 XX
+            ci,cj = ci+di,cj+dj
+            return ci,cj
+        else:                               # 벽이나 다른 사탕이 나오면? : 안움직이고 끝
+            break
+    maps[ci][cj] = moving_candy # exit 아닌 경우, 최종 사탕 위치 표시
+    return ci,cj
+
+
 def dfs(ri,rj,bi,bj,cnt):
-    global N,M,ans
-    ### 현재 상태에 따라 기울기 진행할수 있는지 정하기!!
-    ## 기울기 횟수 이미 10번을 채웠다면? return
-    if cnt == 10:
-        cnt = -1
-        return
-    ## 4가지 경우 중 둘다 exit 아닌 경우에만 기울기 진행 가능!
+    global N,M,ans,maps
+
+    ## 사탕들 exit여부 4가지 경우 중 두개 다 exit 아닌 경우에만 기울기 진행 가능!
     # 1)둘다 exit / 2) 빨강 X and 파랑 O -> return
     if (maps[ri][rj] == 'O' and maps[bi][bj] == 'O') or (maps[ri][rj] != 'O' and maps[bi][bj] == 'O'):
         return
     # 3) 빨 O 파 X -> 정답 상태 도달! answer 갱신, return
     if maps[ri][rj] == 'O' and maps[bi][bj]!='O':
-        ans = min(cnt, ans)
+        ans = min(cnt, ans) if ans!=-1 else cnt # ans가 -1이면 cnt가 ans, 아니면 기존보다 더 작은거
         return
     # 4) 둘 다 exit 아님 -> 기울기 진행
+    ## 기울기 횟수 이미 10번을 채웠다면? return
+    if cnt == 10:
+        return
     for d in range(4):
-        # 1. 해당 방향으로 사탕 옮길 수 있는지 확인 -> 사탕 둘 중 한개라도 '빈칸이거나 exit + next에 방문안해봄' 충족하면 이동 수행 가능!!!
+        # 1. 해당 방향으로 사탕 옮길 수 있는지 확인 -> 사탕 둘 중 한개라도 '다음이 빈칸이거나 exit'을 충족하면 이동 수행 가능!!!
         di,dj = dirs[d][0], dirs[d][1]
         nri,nrj,nbi,nbj = ri+di,rj+dj,bi+di,bj+dj
-        if (maps[nri][nrj] == "." or maps[nri][nrj] == "O" and not visited_r[nri][nri]) or (maps[nbi][nbj] == "." or maps[nbi][nbj] == "O" and not visited_b[nbi][nbj]):
+        if maps[nri][nrj] == "." or maps[nri][nrj] == "O" or maps[nbi][nbj] == "." or maps[nbi][nbj] == "O":
             # 2. 해당 방향으로 사탕 두 개 옮기기
             # 2-1) 먼저 옮길 사탕 구하기
             first = select_first(ri,rj,bi,bj,d)
-            # 2-2) 사탕 두개 옮기기
+            # 2-2) 사탕 두개 옮기기 (기울기 끝난 상태 maps에 기록 / 최종 사탕 위치 반환)
             if first=='R':
-                moved_ri, moved_rj, is_exited_r = move_candy(ri,rj,di,dj)
-                moved_bi, moved_bj, is_exited_b = move_candy(bi,bj,di,dj)
+                moved_ri, moved_rj = move_candy(ri,rj,di,dj,'R')
+                moved_bi, moved_bj = move_candy(bi,bj,di,dj,'B')
             else:
-                moved_bi, moved_bj, is_exited_b = move_candy(bi,bj,di,dj)
-                moved_ri, moved_rj, is_exited_r = move_candy(ri,rj,di,dj)
+                moved_bi, moved_bj = move_candy(bi,bj,di,dj,'B')
+                moved_ri, moved_rj = move_candy(ri,rj,di,dj,'R')
             # 3. 다음 단계로 호출하기
-                # 호출
-                # 호출 전 상태로 되돌리기
+            # 3-1) 호출
+            dfs(moved_ri,moved_rj,moved_bi,moved_bj,cnt+1)
+            # 3-2) 호출 전 상태로 map 되돌리기
+            # - exit이 아닌 사탕 : 예전 위치에 R,B표시, 현재위치를 빈칸으로
+            # - exit인 사탕 : 예전 위치에 R,B표시
+            if maps[moved_ri][moved_rj] !='O':              # exit아닌 경우 현재 위치를 빈칸으로 되돌리기
+                maps[moved_ri][moved_rj] = '.'
+            if maps[moved_bi][moved_bj] !='O':
+                maps[moved_bi][moved_bj] = '.'
+            maps[ri][rj],maps[bi][bj] = 'R','B'             # 예전 위치에 다시 R,B표시 ( 이 두개 순서 바뀌면 안됨,,)
 
 import sys
 input = sys.stdin.readline
 
 N,M = map(int, input().split())
-maps = [ input().strip() for _ in range(N)]
+maps = [ list(input().strip()) for _ in range(N)]
 
 dirs = [(-1,0), (1,0), (0,-1), (0,1)]  # 위, 아래 , 왼, 오
 ri,rj,bi,bj = 0,0,0,0
@@ -69,14 +85,9 @@ for i in range(1,N):
         if maps[i][j] == 'B':
             bi,bj = i,j
 
-# 사탕별 visited 배열
-visited_r = [[False]*M for _ in range(N)]
-visited_b = [[False]*M for _ in range(N)]
 
-visited_r[ri][rj]=True
-visited_b[bi][bj]=True
 dfs(ri,rj,bi,bj, 0) # R위치, B위치, 이때까지 몇번 기울였는지
-
+print(ans)
 '''
 코드트리- 2개의 사탕 / 2015년 하반기 삼성 그룹 오후 2번
 
