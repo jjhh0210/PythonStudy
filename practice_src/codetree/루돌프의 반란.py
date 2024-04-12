8d4ee84481890536fdf12e027cf4e4406e863331
jjhh0210 <parkjihyun0210@gmail.com>
2024-04-12 16:38:50 +0900

    update: CODETREE 루돌프의 반란

--- practice_src/codetree/루돌프의 반란.py
+++ practice_src/codetree/루돌프의 반란.py
@@ -21,6 +21,129 @@
 import sys
 input = sys.stdin.readline
 
+N,M,P,C,D = map(int, input().split())
+maps = [ [0]*(N+1) for _ in range(N+1)] # 1,1부터라서
+dolf_r, dolf_c = map(int, input().split())  # [ r, c ]
+
+santas = { 'pos':[(0,0)]*(P+1),
+           'stop': [0]*(P+1),
+           'score': [0]*(P+1)}
+
+# 위부터 시계방향
+# 상하좌우 : 0,2,4,6
+# 대각선: 1,3,5,7
+dr = [-1,-1,0,1,1,1,0,-1]
+dc = [0,1,1,1,0,-1,-1,-1]
+
+def cal_dist(r,c,r2,c2):
+    return (r-r2)*(r-r2) + (c-c2)*(c-c2)
+
+def crash_santa(st, d, z):# 산타번호, 이동할방향, 몇칸이동할건지(+점수)
+    sr,sc = santas['pos'][st]
+    maps[sr][sc] = 0
+    # 점수 부여
+    santas['score'][st] += z
+    # 기절 처리
+    santas['stop'][st] = 2
+    # 이동
+    # OOR : 탈락 / 산타 있음: 이동, 상호작용 / 빈칸: 이동
+    nsr, nsc = sr+dr[d]*z, sc+dc[d]*z
+    while True:
+        # OOR: 탈락!! maps에서 산타 없애기
+        if not (1<=nsr<=N and 1<=nsc<=N):
+            santas['pos'][st] = (0,0)
+            return
+        # 빈칸: 이동 후 break (위치,maps 업데이트)
+        if maps[nsr][nsc] == 0:
+            maps[nsr][nsc] = st
+            santas['pos'][st] = (nsr,nsc)
+            return
+        else:
+            santas['pos'][st] = (nsr,nsc)
+            # 옮길 산타 번호(st) 업데이트
+            temp = maps[nsr][nsc]   # 원래 있던 산타 빼고
+            maps[nsr][nsc] = st     # 옮길 산타 안착
+            st = temp               # 원래 있던 산타를 옮길 산타로 업데이트!!
+            sr, sc = santas['pos'][st]
+            nsr,nsc = sr+dr[d], sc+dc[d]
+
+for _ in range(P):
+    num, r, c = map(int, input().split())
+    maps[r][c] = num
+    santas['pos'][num] = (r,c)
+
+for _ in range(M):
+    ### 1. 루돌프 이동
+    # 1) 돌진할 산타 선정
+    real_st = None      # 돌진할 산타 (최소 거리)
+    min_dist = None     # 해당 산타와의 현재 거리
+    for st in range(1,P+1):
+        sr, sc = santas['pos'][st]
+        dist = cal_dist(sr, sc, dolf_r, dolf_c)
+        if (sr,sc)!= (0,0):    #탈락안한 산타라면!!
+            if real_st is None or (min_dist,sr,sc) > (dist,santas['pos'][real_st][0], santas['pos'][real_st][1]):
+                real_st = st
+                min_dist = dist
+    # 2) 해당 산타와 가장 가까워지는 방향 정하기
+    sr,sc = santas['pos'][real_st]
+    min_d = 0
+    for d in range(8):
+        dolf_nr, dolf_nc = dolf_r+dr[d], dolf_c+dc[d]
+        if 1<=dolf_nr<=N and 1<=dolf_nc<=N:
+            dist = cal_dist(dolf_nr,dolf_nc,sr,sc)
+            if dist < min_dist:
+                min_dist = dist
+                min_d = d
+    # 루돌프 한칸 이동
+    dolf_r, dolf_c = dolf_r + dr[min_d], dolf_c + dc[min_d]
+    # 충돌 발생시, 산타 이동시키기
+    if (dolf_r, dolf_c) == (sr, sc) :
+        crash_santa(real_st,min_d,C)
+
+    ### 2. 산타 이동
+    for st in range(1,P+1):
+        # 기절 햇거나 탈랙했으면 이동X
+        sr,sc = santas['pos'][st]
+        if santas['stop'][st]>0 or (sr,sc)==(0,0) :
+            continue
+        # 루돌프와 가장 가까워지는 이동 방향 구하기
+        min_dist = cal_dist(sr,sc,dolf_r,dolf_c)
+        min_d = 0
+        for d in [0,2,4,6]: # 상 우 하 좌
+            nsr, nsc = sr+dr[d], sc+dc[d]
+            if 1 <= nsr <= N and 1 <= nsc <= N and maps[nsr][nsc]<=0:    # OOR 아니고, 다른 산타 있지 않고
+                dist = cal_dist(dolf_r, dolf_c, nsr, nsc)
+                if dist < min_dist:
+                    min_dist = dist
+                    min_d = d
+        # min_dist가 갱신이 됐을 경우에만 이동 (더 가까워졌을 경우에만!)
+        if min_dist < cal_dist(sr,sc,dolf_r,dolf_c):
+            maps[sr][sc] = 0
+            sr,sc = sr + dr[min_d], sc + dc[min_d]
+            maps[sr][sc] = st
+            santas['pos'][st] = (sr,sc)
+            # 충돌 발생시, 산타 이동시키기
+            if (dolf_r, dolf_c) == (sr, sc):
+                crash_santa(st, (min_d+4)%8, D)
+
+    # 3. 턴 종료) 탈락 안한 산타 점수 부여, 기절 한 산타 기절 풀기
+    tal_cnt = 0
+    for st in range(1,P+1):
+        r,c = santas['pos'][st]
+        if (r,c) == (0,0):
+            tal_cnt+=1
+        else:
+            santas['score'][st] +=1
+            if santas['stop'][st]>0:
+                santas['stop'][st]-=1
+    # 만약 모두 탈락했다면 게임 종료
+    if tal_cnt == P:
+        break
+
+print(*santas['score'][1:])
+
+
+
 
 
 '''
@@ -30,14 +153,9 @@ input = sys.stdin.readline
 dr = [-1,-1,0,1,1,1,0,-1] 
 dc = [0,1,1,1,0,-1,-1,-1]
 
-santas[p][0~3]
-                0           1       2       3
-santas[p]: [(위치 i,j), 루돌프랑거리, 기절여부, 점수], ... ]    
-
 루돌프랑 거리 계산: cal_dist()
 
 - for _ in range(m):
-    for st in santas: 루돌프랑 산타 간 거리계산 st[1]
     
     1. 루돌프 1칸 돌진 (8방)
     
@@ -49,7 +167,7 @@ santas[p]: [(위치 i,j), 루돌프랑거리, 기절여부, 점수], ... ]
             for문으로 구현
         c. if (루 == 산 같은칸 됨) : 충돌
     2. for 1번~p번 산타 순서대로 한 번씩 움직임 (상하좌우 4방)
-        a. 이동 산타 조건: if 기절 안함⭐️, 게임에서 탈락 안함
+        a. 이동 산타 조건: if 기절 안함, 게임에서 탈락 안함⭐
         b. 루돌프에게 가장 가까워지는 방향으로(상하좌우)!! 가능하면 1칸 이동(멀어지는 방향은x)
             - 가장 가까울 수 있는 방향 조건: 딴 산타있는 곳 아님 and OOR가 아님 and 산-루 거리 제일 줄어듦
             - 가장 가까워질 수 있는 방향 여러개시 우선순위: 상> 우> 하> 좌⭐
