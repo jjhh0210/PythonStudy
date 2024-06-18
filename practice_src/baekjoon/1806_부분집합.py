16ab0039d52963096a4c4fa714e9b13ffdf9329f
jjhh0210 <parkjihyun0210@gmail.com>
2024-06-18 16:33:00 +0900

    update : BOJ 1806_부분집합 - 투포인터

--- practice_src/baekjoon/1806_부분집합.py
+++ practice_src/baekjoon/1806_부분집합.py
@@ -1,7 +1,5 @@
 '''
 - N 짜리 수열 -> 연속된 수 들의 부분합 중 그 합이 S 이상 되는 것 중 가장 짧은 것의 길이
-10 15
-5 1 3 5 10 7 4 9 2 8
 '''
 import sys
 input = sys.stdin.readline
@@ -11,26 +9,25 @@ a = list(map(int, input().split()))
 
 l=0
 summ = 0
-ans = n
-# for r in range(n):
-#     summ += a[r]
-#     if summ>=s:
-#         ans = min(r-l+1,ans)
-#     # l이 움직이는 경우
-#     while l<n and summ>s:
-#         summ-=a[l]
-#         l+=1
-#     if summ>=s:
-#         ans = min(r-l+1,ans)
+ans = float("inf")
 
-r=0
-while 0<=r<n and 0<=l<n:
+for r in range(n):
+    summ += a[r]
     if summ>=s:
-        ans = min(r-l,ans)
+        ans = min(r-l+1,ans)
+    # l이 움직이는 경우
+    while l<n and summ>=s:
         summ-=a[l]
+        ans = min(r-l+1,ans)
         l+=1
-    else:
-        summ+=a[r]
-        r+=1
+if ans == float("inf"):
+    print("0")
+else:
+    print(ans)
 
-print(ans)
+'''
+반례)
+10 30
+1 1 1 1 1 1 15 1 1 15
+정답:4
+'''
